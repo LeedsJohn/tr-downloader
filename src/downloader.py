c872a3bb8f442f303c0f7329ce0b15e87183ee88
John Leeds <leedsjohn6@gmail.com>
2022-08-03 16:36:04 -0500

    Added findRegisteredSpeed and findAccuracy

--- src/downloader.py
+++ src/downloader.py
@@ -13,16 +13,30 @@ class Downloader:
     def download(self, username, raceIndex):
         """
         Download a Type Racer race
-        Returns a list [raceText, typingLog]
+        Returns a dictionary containing with the following keys:
+        date: The date in format {day of the week}, date, Month year, hh:mm:ss
+        textID: The ID of the Type Racer text
+        text: The text that was typed
+        typedText: List in the form ["char typed", ms, add or delete, typo]
+            See Formatter.py for more information
+        raceNum: The race number of the race
+        time: The total amount of time the race took (ms)
+        registeredSpeed: The speed registered to Type Racer
+        accuracy: The accuracy registered to Type Racer
+        unlagSpeed: The true speed of the race without latency
+        adjustedSpeed: The speed of the race accounting for start time
 
-        username (str): the username of the player
-        raceIndex (int): the index of the race to download
+        Parameters:
+            username (str): the username of the player
+            raceIndex (int): the index of the race to download
         """
         url = f"https://data.typeracer.com/pit/result?id=|tr:{username}|{raceIndex}"
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         raceText = soup.find(class_="fullTextStr").text
         typingLog = self.findTypingLog(soup)
+        print(self.findRegisteredSpeed(soup))
+        print(self.findAccuracy(soup))
         return [raceText, typingLog]
 
     def findTypingLog(self, soup):
@@ -46,3 +60,15 @@ class Downloader:
             text = link.get('href')
             if "text_info?id=" in text:
                 return int(text[13:])
+
+    def findRegisteredSpeed(self, soup):
+        for span in soup.find_all("span"):
+            if " WPM" in span.text:
+                return int("".join([c for c in span.text if c.isdigit()]))
+    
+    def findAccuracy(self, soup):
+        prev = ""
+        for td in soup.find_all("td"):
+            if prev == "Accuracy":
+                return td.text.strip()
+            prev = td.text
